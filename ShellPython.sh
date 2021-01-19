f34fbbd995f7ddb52e5cf3a6dab329f2291e9c11
xiayan <39392285+songcheng1@users.noreply.github.com>
2021-01-19 23:05:05 +0800

    Delete ShellPython.sh

--- ShellPython.sh
+++
@@ -1,9 +0,0 @@
-internetNames="data1 data2 data3"
-function pythonName(){
-    nohup python3 /path/main.py $1 10  >> main.log &    
-}
-for internetName in ${internetNames}
-do
-    echo "InternetName ${internetName} start"
-    pythonName ${internetName}
-done
